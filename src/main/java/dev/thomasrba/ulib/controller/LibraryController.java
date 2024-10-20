14ad9d39386b439a1ccc30bf30a184e7daaa9615
Thomas <thomasribalta@gmail.com>
2024-10-20 21:28:07 +0200

    start crud controlleur for lib

--- src/main/java/dev/thomasrba/ulib/controller/LibraryController.java
+++ src/main/java/dev/thomasrba/ulib/controller/LibraryController.java
@@ -1,8 +1,12 @@
 package dev.thomasrba.ulib.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.tomcat.jni.Library;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequestMapping(path = "api/v1/library")
@@ -12,4 +16,17 @@ public class LibraryController {
     public String createLibrary() {
         return "Library created";
     }
+
+    @PutMapping("/{id}")
+    public String updateLibrary(@PathVariable Long id) { return "Library updated"; }
+
+    @GetMapping(value = "/{id}", produces = APPLICATION_JSON_VALUE) // Tmp Map replace with lib object
+    public Map<String, String> getLibrary(@PathVariable Long id) {
+        Map<String, String> library = new HashMap<>();
+        library.put("id", String.valueOf(id));
+        return library;
+    }
+
+    @DeleteMapping("{id}")
+    public String deleteLibrary(@PathVariable Long id) { return "Library deleted"; }
 }
